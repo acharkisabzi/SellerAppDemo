df7288e1b90289377abf479ef4704e7040d85187
Yash <acharkisabzi@gmail.com>
2026-03-31 04:55:20 +0530

    Updated AuthState for new UserModel

--- app/src/main/java/com/example/sellerappdemo/ViewModels/data/AuthState.kt
+++ app/src/main/java/com/example/sellerappdemo/ViewModels/data/AuthState.kt
@@ -3,14 +3,16 @@ package com.example.sellerappdemo.ViewModels.data
 import io.github.jan.supabase.auth.user.UserSession
 
 data class AuthState(
+    val nameInput: String = "",
     val emailInput: String = "",
+    val phoneInput: String = "",
     val passwordInput: String = "",
-    val shopNameInput: String = "",
+    val usernameInput: String = "",
     val areaInput: String = "",
     val isSignUp: Boolean = false,
     val isLoading: Boolean = false,
     val errorMessage: String = "",
     val isAuthenticated: Boolean? = null,
-    val genericError:String = "Something went wrong",
+    val genericError: String = "Something went wrong",
     val session: UserSession? = null
 )
